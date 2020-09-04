d03158470fe2a2169070dd746fc507b080319e31
lodi palle <lodipalle@gmail.com>
2020-09-04 16:37:11 +1000

    cocktail

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_051107) do
+ActiveRecord::Schema.define(version: 2020_09_04_062050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
