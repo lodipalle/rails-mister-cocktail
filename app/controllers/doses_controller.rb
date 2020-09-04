151132371823ad911f9497edc2586da40eaa4704
lodi palle <lodipalle@gmail.com>
2020-09-04 15:35:07 +1000

    mr cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,6 +3,12 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @review = Review.new
+      render "cocktails/show"
+    end
   end
 
   def destroy
